d8c8708b9c67f5624ffe897eb7263b08dfe9384d
versedpro <ultraj0330@gmail.com>
2023-05-10 00:51:17 +0800

    feat: Implement Auth

--- src/App.js
+++ src/App.js
@@ -1,49 +1,10 @@
 // import './App.css';
-import React, { useCallback, useEffect } from "react";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NFT from "./pages/NFT";
 import Home from "./pages/Home";
-import { ModalProvider } from "@particle-network/connect-react-ui";
-import {
-  Chain,
-  ConnectButton,
-  useAccount,
-  useConnectKit,
-  useParticleTheme,
-  useSwitchChains,
-  useParticleProvider,
-  useLanguage,
-} from "@particle-network/connect-react-ui";
-
-import "@particle-network/connect-react-ui/dist/index.css";
-import { LoginOptions } from "@particle-network/auth";
 
 export const App = () => {
-  const account = useAccount();
-  const connectKit = useConnectKit();
-  const { theme, setTheme } = useParticleTheme();
-  const { language, changLanguage } = useLanguage();
-
-  const provider = useParticleProvider();
-
-  const { isSwtichChain, renderChains } = useSwitchChains();
-
-  useEffect(() => {
-    async function chainChanged(chain) {
-      console.log("DEMO-onChainChanged：", chain);
-    }
-    if (connectKit) {
-      connectKit.on("chainChanged", chainChanged);
-      return () => {
-        connectKit.removeListener("chainChanged", chainChanged);
-      };
-    }
-  }, [connectKit]);
-
-  const LogRenderChains = useCallback(() => {
-    console.log("isSwtichChain:", isSwtichChain);
-    console.log("renderChains:", renderChains);
-  }, [renderChains, isSwtichChain]);
   return (
     <div className="">
       <Router>
